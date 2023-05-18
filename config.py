4ec30b16baf33f705a0252f6bab316008e35107a
junqianzi <75289685+junqianzi@users.noreply.github.com>
2023-05-19 00:01:15 +0800

    config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = 'zhuhu00'
+USERNAME = 'junqianzi'
 
 # The repository to add this issue to
-REPO_OWNER = 'zhuhu00'
-REPO_NAME = 'arxiv-daily-notice'
+REPO_OWNER = 'junqianzi'
+REPO_NAME = 'ArXivDaily_Galaxy'
 
 # Set new submission url of subject
-NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
+NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["SLAM","odometry","livox","loam","lidar","loop detection", "nerf","mapping","localization","transformer","autonomous driving"]
+KEYWORD_LIST = ["galaxies","cluster","star formation rate","IMF","dwarf","BCD","star formation history", "star formation","starburst","ISM","molecular clouds","low metallicity"]
